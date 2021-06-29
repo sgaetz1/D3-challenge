f0e7343e0909c25593d34e16dab4b27caaa749b6
Steven Gaetz <gaetz025@umn.edu>
2021-06-28 22:19:17 -0500

    made some edits

--- assets/js/app.js
+++ assets/js/app.js
@@ -162,7 +162,7 @@ function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
 
 
 // Retrieve data from the CSV file and execute everything below
-d3.csv("/../data.csv").then(function(data, err) {
+d3.csv("data.csv").then(function(data, err) {
   if (err) throw err;
 
   // parse data
